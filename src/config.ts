f4d03f54c276d761a3ccc7441cdc8ba63c18bd02
Marcus <mdavidson434@gmail.com>
2025-06-06 22:03:57 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
   website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  author: "Marcus Davidson",
+  desc: "Our school community supported by the PTA",
+  title: "PTA Invictus (Dempsey)",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
